4ae9b603c2819168c308edd1739a8aed1ad46306
zlljjj <705136243@qq.com>
2018-03-12 17:53:04 +0800

    commit
    1.选择高亮跳页面

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,33 +1,46 @@
-import React , { Component } from 'react';
+import React , { Component }from 'react';
 import './header.css'
 import { Link } from 'react-router-dom';
+import history from '../../history'
+
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tabVal: history.location.pathname,
+        };
+    }
+    changeTab = (name) => {
+        this.setState({
+            tabVal:name
+        })
+    }
     render(){
         return(
             <div className = "header">
                 <div className="name">Cynnsound</div>
                 <div className="tab">
                     <Link to="/tech">
-                        <div className = "sub">TECH HOUSE</div>
+                        <div className = {this.state.tabVal === '/tech' ? "sub active":"sub"} onClick={this.changeTab.bind(this,'/tech')}>TECH HOUSE</div>
                     </Link>
                     <Link to="/minimal">
-                        <div className = "sub">MINIMAL</div>
+                        <div className = {this.state.tabVal === '/minimal' ? "sub active":"sub"} onClick={this.changeTab.bind(this,"/minimal")}>MINIMAL</div>
                     </Link>
                     <Link to="/deep">
-                        <div className = "sub">DEEP HOUSE</div>
+                        <div className = {this.state.tabVal === '/deep' ? "sub active":"sub"} onClick={this.changeTab.bind(this,"/deep")}>DEEP HOUSE</div>
                     </Link>
                     <Link to="/techno">
-                        <div className = "sub">TECHNO</div>
+                        <div className = {this.state.tabVal === '/techno' ? "sub active":"sub"} onClick={this.changeTab.bind(this,"/techno")}>TECHNO</div>
                     </Link>
                     <Link to="/after">
-                        <div className = "sub">AFTERHOUR</div>
+                        <div className = {this.state.tabVal === '/after' ? "sub active":"sub"} onClick={this.changeTab.bind(this,"/after")}>AFTERHOUR</div>
                     </Link>
                 </div>
                 <div className="login">login</div>
             </div>
         )
     }
-}
+};
 
 export default Header;
