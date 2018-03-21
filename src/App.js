04a690f9ca978449360d5c10f03f58d520f87808
zlljjj <705136243@qq.com>
2018-03-21 15:36:10 +0800

    commit
    1.页面按需加载

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import Header from './components/Header/header'
 import Audio from './components/Audio/audio'
 import './App.css';
-import { Route , HashRouter , Switch } from 'react-router-dom';
-import After from './pages/After/after';
-import Deep from './pages/Deep/deep';
-import Minimal from './pages/Minimal/minimal';
-import Tech from './pages/Tech/tech';
-import Techno from './pages/Techno/techno';
+import { Route , HashRouter , Switch , Redirect } from 'react-router-dom';
+// import After from './pages/After/after';
+// import Deep from './pages/Deep/deep';
+// import Minimal from './pages/Minimal/minimal';
+// import Tech from './pages/Tech/tech';
+// import Techno from './pages/Techno/techno';
 import 'antd/dist/antd.css'; 
 import { Provider } from 'react-redux';
 import configureStore from './redux/configureStore'
+import asyncComponent from './load';
+
+const After = asyncComponent(() => import("./pages/After/after"));
+const Deep = asyncComponent(() => import("./pages/Deep/deep"));
+const Minimal = asyncComponent(() => import("./pages/Minimal/minimal"));
+const Tech = asyncComponent(() => import("./pages/Tech/tech"));
+const Techno = asyncComponent(() => import("./pages/Techno/techno"));
 
 
 // import './reset.css';
@@ -30,6 +37,7 @@ class App extends Component {
                   <Route path="/deep" component={Deep} />
                   <Route path="/techno" component={Techno} />
                   <Route path="/after" component={After} />
+                  <Redirect from='/' to='/tech'/>
                 </Switch>
             </div>
             <Audio></Audio>
